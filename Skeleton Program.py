08eb3a91592b136fd89ab8e9c6f5e88effda642d
Samuel Heavens <samuel.heavens@ntlworld.com>
2014-04-21 21:54:12 +0100

    Task 3
    Validating the name for a recent score

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -148,7 +148,6 @@ def GetChoiceFromUser():
       ValidGuess = True
     else:
       print("Error. Please enter in a valid choice.")
-      ValidGuess = False
   return Choice
 
 def DisplayEndOfGameMessage(Score):
@@ -182,21 +181,38 @@ def DisplayRecentScores(RecentScores):
   print()
 
 def UpdateRecentScores(RecentScores, Score):
-  PlayerName = GetPlayerName()
-  FoundSpace = False
-  Count = 1
-  while (not FoundSpace) and (Count <= NO_OF_RECENT_SCORES):
-    if RecentScores[Count].Name == '':
-      FoundSpace = True
+  ValidResponse = False
+  AddToScoreTable = input("Do you want to add your score to the high score table? (y or n):").capitalize()
+  while not ValidResponse:
+    if AddToScoreTable == 'Y' or AddToScoreTable == 'Yes':
+      ValidResponse = True
+      ValidName = False
+      PlayerName = ''
+      while not ValidName:
+        PlayerName = GetPlayerName()
+        if not (len(PlayerName) > 0) :
+          print("You must enter something for your name!")
+        else:
+          ValidName = True
+      FoundSpace = False
+      Count = 1
+      while (not FoundSpace) and (Count <= NO_OF_RECENT_SCORES):
+        if RecentScores[Count].Name == '':
+          FoundSpace = True
+        else:
+          Count = Count + 1
+      if not FoundSpace:
+        for Count in range(1, NO_OF_RECENT_SCORES):
+          RecentScores[Count].Name = RecentScores[Count + 1].Name
+          RecentScores[Count].Score = RecentScores[Count + 1].Score
+        Count = NO_OF_RECENT_SCORES
+      RecentScores[Count].Name = PlayerName
+      RecentScores[Count].Score = Score
+    elif AddToScoreTable == 'N' or AddToScoreTable == 'No': 
+      print("Thank you for playing!")
+      ValidResponse = True
     else:
-      Count = Count + 1
-  if not FoundSpace:
-    for Count in range(1, NO_OF_RECENT_SCORES):
-      RecentScores[Count].Name = RecentScores[Count + 1].Name
-      RecentScores[Count].Score = RecentScores[Count + 1].Score
-    Count = NO_OF_RECENT_SCORES
-  RecentScores[Count].Name = PlayerName
-  RecentScores[Count].Score = Score
+      print("Error. Please enter in a valid choice.")  
 
 def PlayGame(Deck, RecentScores):
   LastCard = TCard()
